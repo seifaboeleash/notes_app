71eda6255a0c285495ae44d35064ab69738a08ef
Seif Abo Eleash <seifaboeleish@gmail.com>
2025-08-06 21:07:00 +0300

    refactor multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,9 @@ Bloc.observer = SimpleBlocObserver();
 
 await Hive.initFlutter();
 
-await Hive.openBox<NoteModel>(kNotesBox);
-
 Hive.registerAdapter(NoteModelAdapter());
+
+await Hive.openBox<NoteModel>(kNotesBox);
 
   runApp(const NotesApp());
 }
@@ -24,20 +24,12 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) => AddNoteCubit(),
-          
-        )
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          brightness: Brightness.dark,
-          fontFamily: 'Poppins' ,),
-        home: const NotesView(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: 'Poppins' ,),
+      home: const NotesView(),
     );
   }
 }
